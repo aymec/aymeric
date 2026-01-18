6f70fc1b3c11a24b3983585abba2998ab402cb73
aymec <75215917+aymec@users.noreply.github.com>
2026-01-19 00:18:17 +0900

    Add license to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,6 +24,7 @@ export default function Footer() {
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
+          <div>licensed under the <Link href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1">CC BY 4.0</Link></div>
         </div>
       </div>
     </footer>
